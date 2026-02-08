01ece9d946c327d8e42f6db8f1ee95a0fb80e82c
Vyacheslavlinux <vpetrov@engineer.com>
2026-02-09 02:19:10 +0400

    ready

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import { addComment } from "./listeners.js";
 import { fetchComments } from "./api.js";
 import { updateComments } from "./array.js";
 
+document.querySelector(".comments").innerHTML = '<ul> Продолжается загрузка комментариев...</ul>'
+
 fetchComments().then(data =>{
     updateComments(data);
     renderArray();
 });
 
-// renderArray();
+
 addComment();
