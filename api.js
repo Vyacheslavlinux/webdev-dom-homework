230ebc20a6deb2e1a44b4cf22785dd06e229a944
Vyacheslavlinux <vpetrov@engineer.com>
2026-03-23 21:37:08 +0400

    ready

--- api.js
+++ api.js
@@ -1,4 +1,14 @@
-const host = ' https://wedev-api.sky.pro/api/v1/vyacheslav-petrov'
+const host = ' https://wedev-api.sky.pro/api/v2/vyacheslav-petrov'
+const authHost = 'https://wedev-api.sky.pro/api/user';
+
+export let token = "";
+export const setToken = (newToken) => {
+    token = newToken;
+};
+export let name = "";
+export const setName = (newName) => {
+    name = newName
+}
 
 export const fetchComments = () => {
     return fetch(host + '/comments')
@@ -22,6 +32,9 @@ export const fetchComments = () => {
 export const postComment = (text, name) => {
     return fetch(host +"/comments", {
     method: 'POST',
+    headers: {
+        Authorization: `Bearer ${token}`,
+    },
     body: JSON.stringify({
         text: text,
         name: name,
@@ -38,5 +51,19 @@ export const postComment = (text, name) => {
     })
     .then(() => {
         return fetchComments()
+    })
+}
+
+export const login = (login, password) => {
+    return fetch(authHost + '/login', {
+        method: 'POST',
+        body: JSON.stringify({ login: login, password: password }),
+    })
+};
+
+export const registration = (name, login, password) => {
+    return fetch(authHost, {
+        method: 'POST',
+        body: JSON.stringify({ name: name, login: login, password: password })
     })
 }
--- render.js
+++ render.js
@@ -1,10 +1,18 @@
  import { array } from "./array.js";
+ import { addComment } from "./listeners.js";
  import { initClickLike } from "./listeners.js";
+ import {name, token} from './api.js';
  import { initClickComment } from "./listeners.js";
+import { renderLogin } from "./renderLogin.js";
+ const container = document.querySelector('.container')
+ const LinkToLoginText = '<p><span class="link-login">Чтобы добавить комментарий, авторизуйтесь</span></p'
+
+       
+
  export const renderArray = () => {
  
         let classLike = "";
-
+      
       const arrayHTML = array.map((arr, index) => {
         if (array[index].like == true){classLike = "-active-like"}
         else if (array[index].like == false) {classLike = ""         
@@ -26,18 +34,63 @@
               <button data-index="${index}" class="like-button ${classLike}"></button>
             </div>
           </div>
-        </li>
+        </li>        
         `;
         
       }).join("");
-      
-      
-      container.innerHTML = arrayHTML;
-       
 
-      initClickLike();
-      initClickComment();
+       const addCommentsHtml = `<ul id="container" class="comments" > 
       
+      
+      </ul>
+      <div id="add-form" class="add-form">
+        <input
+          type="text"
+          // id="add-form-name"
+          class="add-form-name"
+          placeholder="Введите ваше имя"
+          readonly
+          value="${name}"
+          id="name-input"
+        />
+        <textarea
+          type="textarea"
+          id="add-form-text"
+          class="add-form-text"
+          placeholder="Введите ваш коментарий"
+          rows="4"
+          
+        ></textarea>
+        <div class="add-form-row">
+          <button id="add-form-button" class="add-form-button">Написать</button>
+        </div >
+             
+      </div>
+      <div class="form-loading" style="display: none; margin-top:20px">Комментарий добавляется...
+
+      </div>`
+
+     const baseHtml = `<ul class="comments">${arrayHTML}</ul>
+     ${token ? addCommentsHtml : LinkToLoginText}
+     ` 
+      
+      
+      
+      
+      
+      container.innerHTML = baseHtml;
+
+      
+       if (token) {
+
+      initClickLike(renderArray);
+      initClickComment();
+      addComment(renderArray);
+       } else {
+      document.querySelector('.link-login').addEventListener('click', () => {
+        renderLogin();
+      })
+      }
     }
 
     
